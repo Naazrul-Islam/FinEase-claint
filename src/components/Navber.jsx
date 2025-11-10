e01751edcb6bec88d02e7c1c359101fe12658471
Naazrul-Islam <hmnazrulislam06@gmail.com>
2025-11-09 23:48:16 -0800

    dropdown added

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -9,7 +9,6 @@ const Navber = () => {
   const { user, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  
   useEffect(() => {
     const html = document.querySelector("html");
     html.setAttribute("data-theme", theme);
@@ -22,8 +21,7 @@ const Navber = () => {
     try {
       await logout();
       console.log("Logged out successfully");
-      
-      
+
       navigate("/");
     } catch (error) {
       console.error("Logout failed:", error);
@@ -34,6 +32,42 @@ const Navber = () => {
     <div className="navbar bg-gradient-to-r from-blue-900/70 to-purple-900/70 text-white backdrop-blur-md shadow-md py-4 sticky top-0 z-50">
       {/* Left */}
       <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 20 20"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex="0"
+            className="menu menu-sm dropdown-content bg-black/30 backdrop-blur-md rounded-box z-50 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <NavLink to={"/"}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/AddTransaction"}>Add Transaction</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/MyTransactions"}>My Transactions</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/Reports"}>Reports</NavLink>
+            </li>
+          </ul>
+        </div>
+
         <Link
           to={"/"}
           className="btn btn-ghost text-2xl flex items-center gap-2 font-bold"
